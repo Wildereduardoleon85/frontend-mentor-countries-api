a7a45c626cdbf71f18680895ac8a96bd57030185
Wilder Leon <wleona@ripley.com>
2023-02-09 20:39:28 -0300

    feat: Add spin animation to chevron icon

--- src/components/Filter/Filter.tsx
+++ src/components/Filter/Filter.tsx
@@ -1,14 +1,20 @@
 import { useState } from 'react'
-import { FiChevronDown } from 'react-icons/fi'
 import { useTheme } from '../../hooks'
+import { ChevronDown } from '../Icons'
 import { Button, ClickAwayWrapper } from '../Ui'
 import styles from './Filter.module.css'
 
 function Filter() {
   const { elementColor } = useTheme()
   const [isActive, setIsActive] = useState<boolean>(false)
+  const [regionSelected, setRegionSelected] = useState<string>('')
 
   const regions = ['America', 'Africa', 'Europa', 'Asia', 'Ocenia']
+
+  function onSelectedRegion(region: string): void {
+    setIsActive(false)
+    setRegionSelected(region)
+  }
 
   return (
     <ClickAwayWrapper onClickAway={() => setIsActive(false)}>
@@ -16,19 +22,21 @@ function Filter() {
         className={styles.filterButton}
         onClick={() => setIsActive(!isActive)}
       >
-        <span>Filter By Region...</span>
-        <FiChevronDown />
+        <span>{regionSelected || 'Filter By Region...'}</span>
+        <ChevronDown
+          className={`${styles.chevronIcon} ${isActive ? styles.spin : ''}`}
+        />
       </Button>
       {isActive && (
         <ul
           style={{ backgroundColor: elementColor }}
           className={styles.dropdown}
         >
-          {regions.map((region) => (
+          {regions.map((region: string) => (
             <li key={region}>
               <Button
                 className={styles.buttonItem}
-                onClick={() => setIsActive(false)}
+                onClick={() => onSelectedRegion(region)}
               >
                 {region}
               </Button>
--- src/components/Icons/index.ts
+++ src/components/Icons/index.ts
@@ -1,3 +1,4 @@
 export { default as MoonIcon } from './MoonIcon'
 export { default as SearchIcon } from './SearchIcon'
 export { default as SunIcon } from './SunIcon'
+export { default as ChevronDown } from './ChevronDown'
